00bb392093b6064503920aeddaaa18be2ecd7081
Devika-Sajeesh <devikasajeesh26@gmail.com>
2026-03-27 10:18:44 +0530

    initial commit'

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        ArrayList<String> items = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (scanner.hasNext()) {
+                items.add(scanner.next());
+            }
+        }
+
+        String searchItem = "";
+        if (scanner.hasNext()) {
+            searchItem = scanner.next();
+        }
+
+        scanner.close();
+
+        Collections.sort(items);
+
+        System.out.println("Sorted Items: " + items);
+        if (items.contains(searchItem)) {
+            System.out.println("Found");
+        } else {
+            System.out.println("Not Found");
+        }
+    }
+}
