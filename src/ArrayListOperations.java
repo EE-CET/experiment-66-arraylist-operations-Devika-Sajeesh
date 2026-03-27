3a70ed6675ca80bf43407eecd4b11ed3cb5817eb
Devika-Sajeesh <devikasajeesh26@gmail.com>
2026-03-27 10:15:57 +0530

    initial commit'

--- src/ArrayListOperations.java
+++ src/ArrayListOperations.java
@@ -5,6 +5,30 @@ import java.util.Scanner;
 public class ArrayListOperations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
+        int n = scanner.nextInt();
+        ArrayList<String> items = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (scanner.hasNext()) {
+                items.add(scanner.next());
+            }
+        }
+
+        String searchItem = "";
+        if (scanner.hasNext()) {
+            searchItem = scanner.next();
+        }
+
+        scanner.close();
+
+        Collections.sort(items);
+
+        System.out.println("Sorted Items: " + items);
+        if (items.contains(searchItem)) {
+            System.out.println("Found");
+        } else {
+            System.out.println("Not Found");
+        }
     }
 }
